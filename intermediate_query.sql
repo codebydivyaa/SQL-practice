c5ab5ea773d92cf32d6eafd21603c2b84ba87c0e
Divya Mali <divyamali4112006@gmail.com>
2026-09-24 14:22:55 +0530

    Add average price and group by category queries

--- intermediate_query.sql
+++ intermediate_query.sql
@@ -3,4 +3,13 @@ SELECT COUNT(*) AS total_products
 FROM products;
 
 
- 
+-- AVERAGE
+SELECT AVG(price) AS average_price
+FROM products;
+
+
+-- GROUP BY 
+SELECT category, COUNT(*) AS total_products
+FROM products
+GROUP BY category;
+
