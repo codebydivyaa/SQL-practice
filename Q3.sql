5b483fc78fae15fcc2b231dcbfceebce6c0c3106
Divya Mali <divyamali4112006@gmail.com>
2026-09-25 09:50:13 +0530

    Update Q3.sql

--- Q3.sql
+++ Q3.sql
@@ -10,5 +10,19 @@ Department VARCHAR(90),
 Salary DECIMAL(10 ,2),
 HireDate DATE
 );
+INSERT INTO employee
+(EmpID, FirstName, LastName, Department, Salary, HireDate)
+VALUES
+(101,'Alice','Johnson','IT',6500, '2020-03-15'),
+(102, 'Mark', 'Rivera', 'HR', 4800, '2019-07-22'),
+(103, 'Sophia', 'Lee', 'Finance', 7200, '2021-01-10'),
+(104, 'Daniel', 'Kim', 'IT', 5800, '2018-11-05'),
+(105, 'Emma', 'Brown', 'Marketing', 5300, '2022-04-18'),
+(106, 'Liam', 'Patel', 'Finance', 6900, '2020-09-29'),
+(107, 'Olivia', 'Garcia', 'HR', 4600, '2017-06-30'),
+(108, 'Noah', 'Thompson', 'IT', 7500, '2023-02-12'),
+(109, 'Ava', 'Martinez', 'Marketing', 5100, '2019-12-02'),
+(110, 'Ethan', 'Davis', 'Finance', 8000, '2016-05-14');
+
 
 
